e91a1916b65feadd87fafda803fd0ef711c96c74
Spidey-y <hammoucheadel0@gmail.com>
2024-03-06 21:05:40 +0000

    fixes

--- misc/shell1/chall/entrypoint.sh
+++ misc/shell1/chall/entrypoint.sh
@@ -3,4 +3,4 @@
 EXEC="/home/ctfer/script.sh"
 PORT=1337
 
-su - ctfer -s /bin/rbash -c "export FLAG='AlphaCTF{that_was_ez_ghir_azha_brk}' && export PATH=/home/ctfer/bin && socat TCP-LISTEN:$PORT,fork SYSTEM:'$EXEC',pty,raw,echo=0,stderr"
+su - ctfer -s /bin/rbash -c "export FLAG='AlphaCTF{that_was_ez_ghir_azha_brk}'  && socat TCP-LISTEN:$PORT,fork SYSTEM:'$EXEC',pty,raw,echo=0,stderr"
--- misc/shell3/chall/entrypoint.sh
+++ misc/shell3/chall/entrypoint.sh
@@ -3,4 +3,4 @@
 EXEC="/home/ctfer/script.sh"
 PORT=1337
 
-su - ctfer -s /bin/rbash -c "export FLAG='AlphaCTF{basssshhhhh_1s_craAaAaAazyYyYyyYyyyy}' && export PATH=/home/ctfer/bin && socat TCP-LISTEN:$PORT,fork SYSTEM:'$EXEC',pty,raw,echo=0,stderr"
+su - ctfer -s /bin/rbash -c "export FLAG='AlphaCTF{basssshhhhh_1s_craAaAaAazyYyYyyYyyyy}' && socat TCP-LISTEN:$PORT,fork SYSTEM:'$EXEC',pty,raw,echo=0,stderr"
